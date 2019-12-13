debbbac28c77a1ee740185e63546af75aadc3b15
SHENO <SHENO@DESKTOP-4UJSGFN>
2019-12-13 05:41:56 -0800

    commited

---
+++ GitSampleService/GitSampleService/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitSampleService.Startup))]
+namespace GitSampleService
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
